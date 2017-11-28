6faf23c774fda4b838d9df7b2c017f4f78042ec8
zplett <zplett@oberlin.edu>
2017-11-27 23:13:49 -0500

    rewriting the merkle tree

---
+++ src/merkle.rs
@@ -0,0 +1,87 @@
+// Included crates
+extern crate ring;
+
+// Use statements
+use std::io;
+use self::ring::digest::{ Digest, Algorithm, SHA256, digest };
+
+/*
+ *
+ * Merkle:
+ *     - This file contains enums, structs, and impls for creating binary trees, merkle trees,
+ *       and iterators to traverse them and access their information.
+ *
+ */
+
+/* Enum for the tree used for allowing multiple classifications of trees:
+ *
+ *     - An empty tree that contains only a hash
+ *     - A tree leaf that contains a hash and a value
+ *     - A tree node ( or root node ) that contains a hash and left and right children
+ *
+ */
+pub enum Tree<T>
+{
+
+    // Empty tree definition
+    Empty
+    {
+        // Empty trees only contain a hash
+        hash: Vec<u8>
+    },
+    // Leaf definition
+    Leaf
+    {
+        // Leaves act as a node with a hash and value but no children
+        hash: Vec<u8>,
+        value: T
+    },
+    // Node ( tree definiton )
+    // The node functions as the normal tree definition as it contains a hash field
+    // and two children.
+    //
+    // Note: Boxes are used for allocating memory on the heap for the left and right tree
+    // values.
+    Node
+    {
+        // Nodes have a hash and left and right children 
+        hash: Vec<u8>,
+        left: Box<Tree<T>>,
+        right: Box<Tree<T>>
+    }
+
+}
+
+// Tree impl 
+impl<T> Tree<T>
+{
+
+    // Empty tree constructor
+    pub fn empty( hash: Digest ) -> Self
+    {
+        // Returns an empty tree with the given hash
+        Tree::Empty{ hash: hash.as_ref().to_vec() }
+    }
+    // Leaf node constructor
+    pub fn leaf( hash: Digest, value: T ) -> Self
+    {
+        // Returns a tree leaf with the given hash and value
+        Tree::Leaf
+        {
+            hash: hash.as_ref().to_vec(),
+            value: value
+        }
+    }
+    // Tree node constructor
+    pub fn node( hash: Digest, left: Box<Tree<T>>, right: Box<Tree<T>> )
+    {
+        // Returns a tree node with the given hash and left and right children
+        Tree::Node
+        {
+            hash: hash.as_ref().to_vec(),
+            left: left,
+            right: right
+        }
+    }
+    
+}
