dca9fd21f85973ac2a22741f1362eb0ec3c5e714
Ezra Goss <ezragoss@me.com>
2017-12-08 15:19:46 -0500

    Committed tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,14 @@
+extern crate off_blockway;
+
+
+use off_blockway::block::*;
+use off_blockway::hash_util::*;
+
+
 // Integration tests for the blockchain
 #[test]
 fn it_works()
 {
+    let block = Block::new( 0, empty_hash() );
     assert!( true );
 }
