87090593030c977a322d63410cf7f303df254f98
Ezra Goss <ezragoss@me.com>
2017-12-15 13:56:53 -0500

    Created length and uid functions for chain

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,12 +3,21 @@ extern crate off_blockway;
 
 use off_blockway::block::*;
 use off_blockway::hash_util::*;
-
+use off_blockway::chain::*;
 
 // Integration tests for the blockchain
 #[test]
 fn it_works()
 {
-    let block = Block::new( 0, empty_hash() );
+
+    let mut chain = Chain::new();
+    println!("{:?}", chain.len() );
+    chain.push( Block::new( 0, empty_hash() ) );
+    chain.push( Block::new( 0, empty_hash() ) );
+    chain.push( Block::new( 0, empty_hash() ) );
+    chain.push( Block::new( 0, empty_hash() ) );
+    println!("{:?}", chain.len() );
+
+    
     assert!( true );
 }
